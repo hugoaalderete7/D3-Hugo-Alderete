f5f0802f228ed9eef5bc5614d53535399bf82a12
Hugo Alderete <hugoaalderete7@gmail.com>
2022-11-02 18:40:24 -0300

    Primer commit

---
+++ src/Componentes/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer () {
+
+    return (
+        <footer>
+            <p>Derechos Reservados</p>
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/View/home/HomePage.js
@@ -0,0 +1,16 @@
+import Header from "../../Componentes/Header";
+import Footer from "../../Componentes/Footer";
+
+function HomePage () {
+
+    return (
+        <div>
+            <Header/>
+            <p>
+                hola mundo
+            </p>
+            <Footer/>
+        </div>
+    )
+}
+export default HomePage;
